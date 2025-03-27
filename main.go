57372e9afa0540851b0195bc3d7b67fb4a2bd765
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2025-03-27 11:05:41 -0400

    feature: implement connect db and gorm

--- main.go
+++ main.go
@@ -1,30 +1,53 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+func loadEnv() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+}
+
+func connectDB() *gorm.DB {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+		os.Getenv("POSTGRES_PORT"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	if err = db.AutoMigrate(); err != nil {
+		panic(err)
+	}
+
+	return db
+}
 
 func bootstrap() *fiber.App {
 	app := fiber.New()
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
-			"message": "Hello world",
-		})
-	})
-
 	return app
 }
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		panic(err)
-	}
-
+	loadEnv()
 	app := bootstrap()
 
-	if err := app.Listen(":9000"); err != nil {
+	if err := app.Listen(":9999"); err != nil {
 		panic(err)
 	}
 }
