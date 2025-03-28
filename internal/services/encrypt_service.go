2fe2d307b84b8da5cdb17cc295b36cc83ffdc031
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2025-03-28 12:30:38 -0400

    feature: implement encrypt service

---
+++ internal/services/encrypt_service.go
@@ -0,0 +1,23 @@
+package services
+
+import "golang.org/x/crypto/bcrypt"
+
+type EncryptService interface {
+	Hash(plain string) (string, error)
+	Verify(hashed string, plain string) error
+}
+
+type encryptService struct{}
+
+func NewEncryptService() encryptService {
+	return encryptService{}
+}
+
+func (s encryptService) Hash(plain string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(plain), 13)
+	return string(hashed), err
+}
+
+func (s encryptService) Verify(hashed string, plain string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashed), []byte(plain))
+}
